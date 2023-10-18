87aa990a63d4ae9ef9a8ffac14e4d4b1dd1d5558
Dario Piotrowicz <dario@cloudflare.com>
2023-10-18 10:54:59 +0100

    add bindings to C3 astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client" />
+
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+
+type ENV = {
+    MY_KV: KVNamespace;
+}
+
+type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>;
+
+declare namespace App {
+    interface Locals extends Runtime {}
+}
